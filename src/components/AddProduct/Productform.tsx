dbbd097c1267b95be08cc0a52eec546fd9502c0c
pmsushil <pmsushil.2011@gmail.com>
2023-06-25 14:45:40 +0530

    formchanges

--- src/components/AddProduct/Productform.tsx
+++ src/components/AddProduct/Productform.tsx
@@ -1,12 +1,12 @@
 import React, { useState, ChangeEvent } from 'react';
-import { Form, Button } from 'react-bootstrap';
+import { Form, Button, FormSelect } from 'react-bootstrap';
 import axios from 'axios';
 import { useParams, useNavigate } from 'react-router-dom';
 
 const Productform: React.FC = () => {
   const [productName, setProductName] = useState('');
-//   const [productPrice, setProductPrice] = useState('');
-//   const [productImage, setProductImage] = useState<File | null>(null);
+  const [productPrice, setProductPrice] = useState('');
+  const [productImage, setProductImage] = useState<File | null>(null);
   const [category, setCategory] = useState('');
   const [dimensions, setDimensions] = useState('');
   const [weight, setWeight] = useState('');
@@ -22,10 +22,10 @@ const Productform: React.FC = () => {
     try {
       const formData = new FormData();
       formData.append('name', productName);
-    //   formData.append('price', productPrice);
-    //   if (productImage) {
-    //     formData.append('image', productImage);
-    //   }
+      formData.append('price', productPrice);
+      if (productImage) {
+        formData.append('image', productImage);
+      }
       formData.append('category', category);
       formData.append('dimensions', dimensions);
       formData.append('weight', weight);
@@ -47,10 +47,7 @@ const Productform: React.FC = () => {
       setProductName('');
       setWeight('');
       navigate(`/products/${adminId}`);
-       if (response.status === 200) {
-        // Redirect to the respective admin's dashboard
-        navigate(`/products/${adminId}`);
-      } 
+     
 
       console.log(response.data);
       // Handle success, e.g., show a success message or redirect to another page
@@ -61,10 +58,10 @@ const Productform: React.FC = () => {
     
   };
 
-//   const handleImageChange = (e: ChangeEvent<HTMLInputElement>) => {
-//     const file = e.target.files?.[0];
-//     setProductImage(file || null);
-//   };
+  const handleImageChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+    setProductImage(file || null);
+  };
 
   return (
     <Form onSubmit={handleFormSubmit} style={{display:"flex", flexDirection:"column", gap:"20px", justifyItems:"center", paddingLeft:"10px"}}>
@@ -72,83 +69,125 @@ const Productform: React.FC = () => {
         <Form.Label> Name</Form.Label>
         <Form.Control
           type="text"
-          placeholder="Enter product name"
+          placeholder=""
           value={productName}
           onChange={(e) => setProductName(e.target.value)}
           style={{width:"30%"}}
         />
       </Form.Group>
 
-      {/* <Form.Group controlId="productPrice" style={{display:"flex", gap:"10px"}}>
-        <Form.Label> Price</Form.Label>
-        <Form.Control
-          type="text"
-          placeholder="Enter product price"
-          value={productPrice}
-          onChange={(e) => setProductPrice(e.target.value)}
-          style={{width:"30%"}}
-        />
-      </Form.Group> */}
-
-      {/* <Form.Group controlId="productImage" style={{display:"flex", gap:"10px"}}>
-        <Form.Label> Image</Form.Label>
-        <Form.Control type="file"
-        style={{width:"30%"}}
-         onChange={handleImageChange} />
-      </Form.Group> */}
-
       <Form.Group controlId="productCategory" style={{display:"flex", gap:"10px"}}>
-        <Form.Label>Category</Form.Label>
+        <Form.Label>Material Specifications</Form.Label>
         <Form.Control
           type="text"
-          placeholder="Enter product Category"
+          placeholder=""
           value={category}
           onChange={(e) => setCategory(e.target.value)}
           style={{width:"30%"}}
         />
       </Form.Group>
 
-      <Form.Group controlId="productDimensions" style={{display:"flex", gap:"10px"}}>
-        <Form.Label>Dimensions</Form.Label>
-        <Form.Control
-          type="text"
-          placeholder="Enter product Dimensions"
-          value={dimensions}
-          onChange={(e) => setDimensions(e.target.value)}
-          style={{width:"30%"}}
-        />
+
+      <Form.Group controlId="productImage" style={{display:"flex", gap:"10px"}}>
+        <Form.Label> Image</Form.Label>
+        <Form.Control type="file"
+        style={{width:"30%"}}
+         onChange={handleImageChange} />
       </Form.Group>
-      <Form.Group controlId="productWeight" style={{display:"flex", gap:"10px"}}>
-        <Form.Label>Weight</Form.Label>
-        <Form.Control
-          type="text"
-          placeholder="Enter product Weight"
-          value={weight}
-          onChange={(e) => setWeight(e.target.value)}
-          style={{width:"30%"}}
-        />
-      </Form.Group>
+
       <Form.Group controlId="productDescription" style={{display:"flex", gap:"10px"}}>
         <Form.Label>Description</Form.Label>
         <Form.Control
           type="text"
-          placeholder="Enter product Description"
+          placeholder=""
           value={description}
           onChange={(e) => setDescription(e.target.value)}
           style={{width:"30%"}}
         />
       </Form.Group>
 
-      <Form.Group controlId="productGtin" style={{display:"flex", gap:"10px"}}>
-        <Form.Label>GTIN</Form.Label>
+      <Form.Group controlId="productPrice" style={{display:"flex", gap:"10px"}}>
+        <Form.Label> Price</Form.Label>
         <Form.Control
           type="text"
-          placeholder="Enter product GTIN"
+          placeholder=""
+          value={productPrice}
+          onChange={(e) => setProductPrice(e.target.value)}
+          style={{width:"30%"}}
+        />
+      </Form.Group>
+
+      <Form.Group controlId="productGtin" style={{display:"flex", gap:"10px"}}>
+        <Form.Label>Units</Form.Label>
+        <Form.Select style={{width:"30%"}}>
+        <option >Select the Unit</option>
+        <option value="1">Euro</option>
+        <option value="2">Pound</option>
+        <option value="3">Dollar</option>
+
+        </Form.Select>
+      </Form.Group>
+     
+
+      <Form.Group controlId="productDimensions" style={{display:"flex", gap:"10px"}}>
+        <Form.Label>Dimensions</Form.Label>
+        <Form.Control
+          type="text"
+          placeholder=""
+          value={dimensions}
+          onChange={(e) => setDimensions(e.target.value)}
+          style={{width:"30%"}}
+        />
+      </Form.Group>
+
+     
+
+      <Form.Group controlId="productWeight" style={{display:"flex", gap:"10px"}}>
+        <Form.Label>Weight</Form.Label>
+        <Form.Control
+          type="text"
+          placeholder=""
+          value={weight}
+          onChange={(e) => setWeight(e.target.value)}
+          style={{width:"30%"}}
+        />
+      </Form.Group>
+    
+
+    
+
+      <Form.Group controlId="productGtin" style={{display:"flex", gap:"10px"}}>
+        <Form.Label>SKU</Form.Label>
+        <Form.Control
+          type="text"
+          placeholder=""
           value={gtin}
           onChange={(e) => setGtin(e.target.value)}
           style={{width:"30%"}}
         />
       </Form.Group>
+
+      <Form.Group controlId="productGtin" style={{display:"flex", gap:"10px"}}>
+        <Form.Label>Location</Form.Label>
+        <Form.Control
+          type="text"
+          placeholder=""
+          value={gtin}
+          onChange={(e) => setGtin(e.target.value)}
+          style={{width:"30%"}}
+        />
+      </Form.Group>
+      <Form.Group controlId="productGtin" style={{display:"flex", gap:"10px"}}>
+        <Form.Label>Stock</Form.Label>
+        <Form.Control
+          type="text"
+          placeholder=""
+          value={gtin}
+          onChange={(e) => setGtin(e.target.value)}
+          style={{width:"30%"}}
+        />
+      </Form.Group>
+    
       
       
 
