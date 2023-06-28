2d104b19b214211560773ece837995ba6b846006
pmsushil <pmsushil.2011@gmail.com>
2023-06-28 12:39:30 +0530

    new

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,106 +1,74 @@
-import React, { useState, useEffect } from "react";
-import { Card, Container, Row,Col, Button,Form, Carousel } from "react-bootstrap";
-import Imagenew from "./../assets/Images/938.jpg"
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import { Link, useParams } from 'react-router-dom';
 import InputGroup from 'react-bootstrap/InputGroup';
+import { Card, Container, Row,Col, Button,Form, Carousel } from "react-bootstrap";
+import Imagenew from "./../assets/Images/938.jpg"
 
 
-function ProductList() {
-const [tenantData, setTenantData] = useState([]);
-const [productData, setProductData] = useState({});
-const [searchQuery, setSearchQuery] = useState("");
-const { adminId } = useParams();
 
-useEffect(() => {
-// Fetch the main API to get the tenant data
-fetch("http://aidiz.co.uk:8080/create_tenant/")
-.then((response) => response.json())
-.then((data) => {
-setTenantData(data);
-})
-.catch((error) => {
-console.error("Error fetching tenant data:", error);
-});
-}, []);
+const ProductList = () => {
+  const [products, setProducts] = useState([]);
+  const [searchQuery, setSearchQuery] = useState("");
 
-useEffect(() => {
-// Fetch product data for each tenant
-tenantData.forEach((tenant) => {
-fetch(`http://${tenant.schema_name}.aidiz.co.uk:8080/Product/`)
-//http://${tenant.schema_name}.aidiz.co.uk:8080/Product/
-.then((response) => response.json())
-.then((data) => {
-setProductData((prevProductData) => ({
-...prevProductData,
-[tenant.schema_name]: data,
-}));
-})
-.catch((error) => {
-console.error(`Error fetching product data for ${tenant.schema_name}:`, error);
-});
-});
-}, [tenantData]);
+  const { adminId } = useParams();
 
-return (
-<div style={{ padding:"12px"}}>
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const response = await axios.get((`http://${adminId}.aidiz.co.uk:8080/Product/`));
+        setProducts(response.data);
+      } catch (error) {
+        console.error('Error fetching products:', error);
+      }
+    };
+
+    fetchProducts();
+  }, []);
+
+  return (
+    <>
+    <div style={{ padding:"12px"}}>
 
     <div style={{display:"flex", gap:"10px", justifyContent:"space-between"}}>
         
-    {/* <input
-        type="text"
-        placeholder="Search products"
-        value={searchQuery}
-        onChange={(e) => setSearchQuery(e.target.value)}
-      /> */}
-        <InputGroup className="mb-3" style={{width:"50%"}}>
-      <InputGroup.Text style={{background:"#80ced6"}}>Search Products</InputGroup.Text>
-      <Form.Control aria-label="Search..." 
-       placeholder="Search..."
-       value={searchQuery}
-       onChange={(e) => setSearchQuery(e.target.value)}/>
-    </InputGroup>
-      <Link to={`/addproduct/${adminId}`}>
-        <Button style={{ justifyContent:"end"}}>Add Product</Button>
-        </Link>
-    </div>
-   
-    <h2>Latest Product</h2>
+        {/* <input
+            type="text"
+            placeholder="Search products"
+            value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)}
+          /> */}
+            <InputGroup className="mb-3" >
+          <InputGroup.Text style={{background:"#80ced6"}}>Search Products</InputGroup.Text>
+          <Form.Control aria-label="Search..." 
+           placeholder="Search..."
+           value={searchQuery}
+           onChange={(e) => setSearchQuery(e.target.value)}/>
+        </InputGroup>
+        
+        </div>
 
-{tenantData.map((tenant) => (
-<div key={tenant.schema_name}>
+      <h1>Latest Products </h1>
+      <Row>
+        {products.filter((product) => product.name.toLowerCase().includes(searchQuery.toLowerCase()) ).map((product) => (
+           <Col key={product.id} sm={12} md={6} lg={4} xl={3}>
+           <Card className='my-3 p-5 rounded'>
+             <Card.Img src={Imagenew} />
+             <Card.Body>
+               <Card.Title as='div'>
 
-{productData[tenant.schema_name] ? (
-  <>
-  <div>
-   
-  </div>
-  <Row style={{}}>
-        {productData[tenant.schema_name].filter((product) => product.name.toLowerCase().includes(searchQuery.toLowerCase()) ||
-          tenant.schema_name.toLowerCase().includes(searchQuery.toLowerCase())
-        ).map((product) => (
-          <Col key={product.Cement} sm={12} md={6} lg={4} xl={3}>
-            <Card className='my-3 p-5 rounded'>
-              <Card.Img src={Imagenew} />
-              <Card.Body>
-                <Card.Title as='div'>
-                <Link to={`/product/${product.name}/${tenant.schema_name}`}><strong style={{ textTransform: "capitalize" }}>{product.name}</strong>
-                </Link>
-                <br />
-                  <h6 style={{ textTransform: "capitalize" }}>Product from:{tenant.schema_name}</h6>
-                </Card.Title>
-              </Card.Body>
-            </Card>
-          </Col>
-
+                <Link to={`/product/${product.id}`}>
+               <strong style={{ textTransform: "capitalize" }}>{product.name}</strong>
+               </Link>
+               </Card.Title>
+             </Card.Body>
+           </Card>
+         </Col>
         ))}
-      </Row></>
-) : (
-<p>Loading products...</p>
-)}
-</div>
-))}
-</div>
-);
-}
+      </Row>
+    </div>
+    </>
+  );
+};
 
 export default ProductList;
