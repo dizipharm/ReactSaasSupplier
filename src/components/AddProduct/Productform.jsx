17eb286b1fd9408bf5f9b684536b530879afc08b
Chandrasekhar <chandrasekhar.reddy@bestinet.com.my>
2023-08-02 19:32:19 +0100

    form changes

--- src/components/AddProduct/Productform.jsx
+++ src/components/AddProduct/Productform.jsx
@@ -4,69 +4,150 @@ import axios from 'axios';
 import { useParams, useNavigate } from 'react-router-dom';
 import { getElementGroupList, getMaterialsList, getStructuralList } from '../Services/materialService';
 
+ 
+
 const Productform = () => {
-  const [productName, setProductName] = useState('');
-  const [productPrice, setProductPrice] = useState('');
-  const [productImage, setProductImage] = useState(null);
-  const [category, setCategory] = useState('');
-  const [dimensions, setDimensions] = useState('');
-  const [weight, setWeight] = useState('');
-  const [description, setDescription] = useState('');
-  const [gtin, setGtin] = useState('');
 
+ 
+
+  const [formData, setFormData] = useState({
+
+  
+
+    // Initialize state for the form data you want to send in the request
+
+ 
+
+    // Example:
+
+ 
+
+productName: '',
+productDescription: '',
+sku: '',
+materialspecification:'',
+price:'',
+stock:'',
+units:''
+
+ 
+
+ 
+
+  });
+
+ 
+
+ 
+
+ 
   const [materialSpecList, setMaterialSpecList] = useState([]);
+ 
 
-  const navigate = useNavigate();
-  const { adminId } = useParams();
+  const handleFormSubmit = (event) => {
 
-  const handleFormSubmit = async (e) => {
-    e.preventDefault();
+ 
 
-    try {
-      const formData = new FormData();
-      formData.append('name', productName);
-      formData.append('price', productPrice);
-      if (productImage) {
-        formData.append('image', productImage);
-      }
-      formData.append('category', category);
-      formData.append('dimensions', dimensions);
-      formData.append('weight', weight);
-      formData.append('description', description);
-      formData.append('gtin', gtin);
+    event.preventDefault();
 
-      const apiUrl = `http://${adminId}.aidiz.co.uk:8080/Product/`;
+ 
+    // Replace 'YOUR_API_ENDPOINT' with your actual API endpoint
 
-      const response = await axios.post(apiUrl, formData, {
-        headers: {
-          'Content-Type': 'multipart/form-data',
-        },
+ 
+
+    // const apiEndpoint = 'https://example.com/api/endpoint';
+
+ 
+
+    const apiEndpoint = 'http://localhost:8082/api/product';
+
+  
+
+     // Make the POST request
+
+ 
+
+    axios.post(apiEndpoint, formData)
+
+ 
+
+      .then((response) => {
+
+ 
+
+        // Handle the response data here if needed
+
+ 
+
+        console.log('Response:', response.data);
+
+ 
+
+      })
+
+ 
+
+      .catch((error) => {
+
+ 
+
+        // Handle any errors that occurred during the request
+
+ 
+
+        console.error('Error:', error);
+
+ 
+
       });
 
-      setCategory('');
-      setDescription('');
-      setDimensions('');
-      setGtin('');
-      setProductName('');
-      setWeight('');
-      navigate(`/products/${adminId}`);
-     
+ 
 
-      console.log(response.data);
-      // Handle success, e.g., show a success message or redirect to another page
-    } catch (error) {
-      console.error(error);
-      // Handle error, e.g., show an error message
-    }
-    
   };
 
-  const handleImageChange = (e) => {
+ 
+
+ 
+
+ 
+
+ 
+
+  const handleInputChange = (event) => {
+
+ 
+
+    const { name, value } = event.target;
+
+ 
+
+    setFormData({
+
+ 
+
+      ...formData,
+
+ 
+
+      [name]: value,
+
+ 
+
+    });
+
+ 
+
+  };
+
+ 
+
+
+const handleImageChange = (e) => {
     const file = e.target.files?.[0];
     setProductImage(file || null);
   };
 
-  useEffect(() => {
+useEffect(() => {
     getMaterialsList()
     .then((res) => {
       setMaterialSpecList(res.data);
@@ -78,139 +159,87 @@ const Productform = () => {
     });
   })
 
+
+ 
+
   return (
-    <Form onSubmit={handleFormSubmit} style={{display:"flex", flexDirection:"column", gap:"20px", justifyItems:"center", zIndex:"2px" }}>
-      <Form.Group controlId="productName" style={{display:"flex", gap:"10px"}}>
-        <Form.Label> Name</Form.Label>
-        <Form.Control
-          type="text"
-          placeholder=""
-          value={productName}
-          onChange={(e) => setProductName(e.target.value)}
-           style={{width:"30%"}}
-        />
-      </Form.Group>
 
-      <Form.Group controlId="productCategory" style={{display:"flex", gap:"10px"}}>
-        <Form.Label>Material Specifications</Form.Label>
-        <Form.Select  style={{width:"30%"}} 
-        onChange={(e) => setCategory(e.target.value)} 
-        value={category}
-         >
-        <option >Select the Material</option>
+ 
+
+<form onSubmit={handleFormSubmit} style={{display:"flex", flexDirection:"column", gap:"20px", justifyItems:"center", zIndex:"2px" }}>
+
+ 
+
+      {/* Add your form fields here */}
+
+ 
+
+<input type="text" name="productName" value={formData.productName} onChange={handleInputChange} placeholder="ProductName" />
+
+ 
+
+<input type="text" name="productDescription" value={formData.productDescription} onChange={handleInputChange} placeholder="Product Description" />
+
+ 
+
+<input type="text" name="sku" value={formData.message} onChange={handleInputChange} placeholder="sku"></input>
+
+ 
+
+<input type="number" name="price" value={formData.price} onChange={handleInputChange} placeholder="price"></input>
+
+ 
+
+{/* <input type="text" name="stock" value={formData.stock} onChange={handleInputChange} placeholder="stock"></input> */}
+
+<select
+value={formData.stock} onChange={handleInputChange}>
+<option disabled>Stock Update</option>
+<option>In stock</option>
+<option>Out of stock</option>
+</select>
+ 
+
+<select onChange={handleInputChange}
+value={formData.materialspecification}>
+<option disabled>Select the Material</option>
         {materialSpecList.map((material, index) => (
-                    <option key={index + 1} value={material['material_specification']+ ';' + (index+1)}>{material['material_specification']}</option>
+<option key={index + 1} value={material['material_specification']+ ';' + (index+1)}>{material['material_specification']}</option>
                   ))}
+</select>
 
-        </Form.Select>
-      </Form.Group>
+ 
 
+<select onChange={handleInputChange}
+value={formData.units}>
+<option >Select the Unit</option>
+<option value="1">Euro</option>
+<option value="2">Pound</option>
+<option value="3">Dollar</option>
+</select>
 
-      <Form.Group controlId="productImage" style={{display:"flex", gap:"10px"}}>
-        <Form.Label> Image</Form.Label>
-        <Form.Control type="file"
-         style={{width:"30%"}}
-         onChange={handleImageChange} />
-      </Form.Group>
+ 
 
-      <Form.Group controlId="productDescription" style={{display:"flex", gap:"10px"}}>
-        <Form.Label>Description</Form.Label>
-        <Form.Control
-          type="text"
-          placeholder=""
-          value={description}
-          onChange={(e) => setDescription(e.target.value)}
-           style={{width:"30%"}}
-        />
-      </Form.Group>
+<button type="submit">Submit</button>
 
-      <Form.Group controlId="productPrice" style={{display:"flex", gap:"10px"}}>
-        <Form.Label> Price</Form.Label>
-        <Form.Control
-          type="text"
-          placeholder=""
-          value={productPrice}
-          onChange={(e) => setProductPrice(e.target.value)}
-           style={{width:"30%"}}
-        />
-      </Form.Group>
+ 
 
-      <Form.Group controlId="productGtin" style={{display:"flex", gap:"10px"}}>
-        <Form.Label>Units</Form.Label>
-        <Form.Select  style={{width:"30%"}}  >
-        <option >Select the Unit</option>
-        <option value="1">Euro</option>
-        <option value="2">Pound</option>
-        <option value="3">Dollar</option>
+</form>
 
-        </Form.Select>
-      </Form.Group>
-     
+ 
 
-      {/* <Form.Group controlId="productDimensions" style={{display:"flex", gap:"10px"}}>
-        <Form.Label>Dimensions</Form.Label>
-        <Form.Control
-          type="text"
-          placeholder=""
-          value={dimensions}
-          onChange={(e) => setDimensions(e.target.value)}
-           style={{width:"30%"}}
-        />
-      </Form.Group> */}
-
-     
-
-      {/* <Form.Group controlId="productWeight" style={{display:"flex", gap:"10px"}}>
-        <Form.Label>Weight</Form.Label>
-        <Form.Control
-          type="text"
-          placeholder=""
-          value={weight}
-          onChange={(e) => setWeight(e.target.value)}
-           style={{width:"30%"}}
-        />
-      </Form.Group>
-     */}
-
-    
-
-      {/* <Form.Group controlId="productGtin" style={{display:"flex", gap:"10px"}}>
-        <Form.Label>SKU</Form.Label>
-        <Form.Control
-          type="text"
-          placeholder=""
-          value={gtin}
-          onChange={(e) => setGtin(e.target.value)}
-           style={{width:"30%"}}
-        />
-      </Form.Group> */}
-
-      {/* <Form.Group controlId="productGtin" style={{display:"flex", gap:"10px"}}>
-        <Form.Label>Location</Form.Label>
-        <Form.Control
-          type="text"
-          placeholder=""
-          value={gtin}
-          onChange={(e) => setGtin(e.target.value)}
-           style={{width:"30%"}}
-        />
-      </Form.Group> */}
-      <Form.Group controlId="productGtin" style={{display:"flex", gap:"10px"}}>
-        <Form.Label>Stock</Form.Label>
-        <Form.Control
-          type="text"
-          placeholder=""
-          value={gtin}
-          onChange={(e) => setGtin(e.target.value)}
-           style={{width:"30%"}}
-        />
-      </Form.Group>
-    
-      <Button variant="primary" type="submit" style={{width:"15%"}} >
-        Add Product
-      </Button>
-    </Form>
   );
+
+ 
+
 };
+
+ 
+
+ 
+
+ 
+
+ 
 
 export default Productform;
