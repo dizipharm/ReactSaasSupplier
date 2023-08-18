36d86be757316c2fcd3c8380237c7c3ef1386a00
pmsushil <pmsushil.2011@gmail.com>
2023-08-18 12:53:48 +0530

    new changes in dashboard

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -8,53 +8,53 @@ const Dashboard = () => {
   return (
     <div>
       <div style={{display:'flex',gap:'1rem',height:'12rem'}}>
-      <Card style={{ width: '18rem',marginTop:'1rem',padding:'1rem',backgroundImage:'linear-gradient(to right, #f0ffff, #483d8b)' }}>
-      <Card.Title>Products</Card.Title>
+      <Card style={{ width: '18rem',marginTop:'1rem',padding:'1rem',backgroundImage:'linear-gradient(to right, #9370db, #483d8b)' }}>
+      <Card.Title style={{color:'white'}}>Products</Card.Title>
       <Card.Body >
         {/* <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle> */}
         {/* <Card.Text>
           Some quick example text to build on the card title and make up the
           bulk of the card's content.
         </Card.Text> */}
-        <Card.Link style={{textDecoration:'none'}} href="/products">All products</Card.Link>
+        <Card.Link style={{textDecoration:'none',color:'#264348'}} href="/products">All products</Card.Link>
         <br></br>
 
-        <Card.Link style={{textDecoration:'none'}} href="/addproduct">Add product</Card.Link>
+        <Card.Link style={{textDecoration:'none',color:'#264348'}} href="/addproduct">Add product</Card.Link>
         <br></br>
 
-        <Card.Link style={{textDecoration:'none'}} href="/upload">Upload</Card.Link>
+        <Card.Link style={{textDecoration:'none',color:'#264348'}} href="/upload">Upload</Card.Link>
 
 
       </Card.Body>
     </Card>
-    <Card style={{ width: '18rem',marginTop:'1rem',padding:'1rem', backgroundImage:'linear-gradient(to right, #f0ffff, #483d8b)'}}>
-    <Card.Title>Order Management</Card.Title>
+    <Card style={{ width: '18rem',marginTop:'1rem',padding:'1rem', backgroundImage:'linear-gradient(to right, #9370db, #483d8b)'}}>
+    <Card.Title  style={{color:'white'}}>Order Management</Card.Title>
       <Card.Body >
         {/* <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle> */}
         {/* <Card.Text>
           Some quick example text to build on the card title and make up the
           bulk of the card's content.
         </Card.Text> */}
-        <Card.Link style={{textDecoration:'none'}} href="/ordermanagement">My Orders</Card.Link>
+        <Card.Link style={{textDecoration:'none',color:'#264348'}} href="/ordermanagement">My Orders</Card.Link>
         <br></br>
-        <Card.Link style={{textDecoration:'none'}} href="/ordermanagement">New Orders</Card.Link>
+        <Card.Link style={{textDecoration:'none',color:'#264348'}} href="/ordermanagement">New Orders</Card.Link>
         <br></br>
-        <Card.Link style={{textDecoration:'none'}} href="/ordermanagement">Accepted Orders</Card.Link>
+        <Card.Link style={{textDecoration:'none',color:'#264348'}} href="/ordermanagement">Accepted Orders</Card.Link>
         <br></br>
-        <Card.Link style={{textDecoration:'none'}} href="/ordermanagement">Rejected Orders</Card.Link>
+        <Card.Link style={{textDecoration:'none',color:'#264348'}} href="/ordermanagement">Rejected Orders</Card.Link>
 
 
       </Card.Body>
     </Card >
-    <Card style={{ width: '18rem',marginTop:'1rem',padding:'1rem' ,backgroundImage:'linear-gradient(to right, #f0ffff, #483d8b)'}}>
-    <Card.Title>Location</Card.Title>
+    <Card style={{ width: '18rem',marginTop:'1rem',padding:'1rem' ,backgroundImage:'linear-gradient(to right, #9370db, #483d8b)'}}>
+    <Card.Title  style={{color:'white'}}>Location</Card.Title>
       <Card.Body >
         {/* <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle> */}
         {/* <Card.Text>
           Some quick example text to build on the card title and make up the
           bulk of the card's content.
         </Card.Text> */}
-        <Card.Link style={{textDecoration:'none'}} href="/location">Manage Location</Card.Link>
+        <Card.Link style={{textDecoration:'none',color:'#264348'}} href="/location">Manage Location</Card.Link>
         
 
 
